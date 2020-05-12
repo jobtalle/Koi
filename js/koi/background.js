dd9d891f15ed147e3a8faabb94f5d52866c36ae9
Job <jobtalle@hotmail.com>
2020-05-12 15:29:49 +0200

    Created ugly sand shader

--- js/koi/background.js
+++ js/koi/background.js
@@ -1,14 +1,27 @@
 /**
  * The background of the scene
  * @param {WebGLRenderingContext} gl A WebGL rendering context
+ * @param {Sand} sand The sand renderer
  * @param {Number} width The width in pixels
  * @param {Number} height The height in pixels
  * @constructor
  */
-const Background = function(gl, width, height) {
-    this.renderTarget = new RenderTarget(gl, width, height, gl.RGB, gl.NEAREST);
+const Background = function(gl, sand, width, height) {
+    this.bottom = new RenderTarget(gl, width, height, gl.RGB, gl.NEAREST);
     this.width = width;
     this.height = height;
+
+    this.paintSand(sand);
+};
+
+/**
+ * Paint sand on the bottom
+ * @param {Sand} sand The sand renderer
+ */
+Background.prototype.paintSand = function(sand) {
+    this.bottom.target();
+
+    sand.write(this.width, this.height);
 };
 
 /**
@@ -16,7 +29,7 @@ const Background = function(gl, width, height) {
  * @param {Primitives} primitives The primitives renderer
  */
 Background.prototype.render = function(primitives) {
-    primitives.setTexture(this.renderTarget.texture);
+    primitives.setTexture(this.bottom.texture);
     primitives.drawQuad(0, 0, this.width, this.height);
     primitives.flush();
 };
@@ -25,5 +38,5 @@ Background.prototype.render = function(primitives) {
  * Free all resources maintained by the background
  */
 Background.prototype.free = function() {
-    this.renderTarget.free();
+    this.bottom.free();
 };
--- js/render/sand.js
+++ js/render/sand.js
@@ -4,41 +4,78 @@
  * @constructor
  */
 const Sand = function(gl) {
+    this.gl = gl;
+    this.buffer = gl.createBuffer();
     this.program = new Shader(
         gl,
         this.SHADER_VERTEX,
         this.SHADER_FRAGMENT,
-        [],
-        []);
+        ["size"],
+        ["position", "uv"]);
+
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffer);
+    this.gl.bufferData(
+        this.gl.ARRAY_BUFFER,
+        new Float32Array([
+            -1, -1, 0, 0,
+            -1, 1, 0, 1,
+            1, 1, 1, 1,
+            1, -1, 1, 0
+        ]),
+        gl.STATIC_DRAW);
 };
 
 Sand.prototype.SHADER_VERTEX = `#version 100
 attribute vec2 position;
+attribute vec2 uv;
+
+varying mediump vec2 iUv;
 
 void main() {
-  gl_Position = vec(position, 0.0, 1.0);
+  iUv = uv;
+  
+  gl_Position = vec4(position, 0.0, 1.0);
 }
 `;
 
 Sand.prototype.SHADER_FRAGMENT = `#version 100
+uniform mediump vec2 size;
+
+varying mediump vec2 iUv;
+
+mediump float random(mediump vec2 n) { 
+  return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+}
+
 void main() {
-  gl_FragColor = vec4(1.0);
+  mediump float lightness = 0.6 + 0.2 * pow(random(iUv * size), 4.5);
+  gl_FragColor = vec4(vec2(lightness), 0.0, 1.0);
 }
 `;
 
 /**
  * Write a sand texture
- * @param {Primitives} primitives The primitives renderer
  * @param {Number} width The width in pixels
  * @param {Number} height The height in pixels
  */
-Sand.prototype.write = function(primitives, width, height) {
+Sand.prototype.write = function(width, height) {
+    this.program.use();
 
+    this.gl.uniform2f(this.program.uSize, width, height);
+
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffer);
+    this.gl.enableVertexAttribArray(this.program.aPosition);
+    this.gl.vertexAttribPointer(this.program.aPosition, 2, this.gl.FLOAT, false, 16, 0);
+    this.gl.enableVertexAttribArray(this.program.aUv);
+    this.gl.vertexAttribPointer(this.program.aUv, 2, this.gl.FLOAT, false, 16, 8);
+
+    this.gl.drawArrays(this.gl.TRIANGLE_FAN, 0, 4);
 };
 
 /**
  * Free all resources maintained by this system
  */
 Sand.prototype.free = function() {
+    this.gl.deleteBuffer(this.buffer);
     this.program.free();
 };
