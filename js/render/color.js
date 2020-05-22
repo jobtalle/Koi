a0475aa36bba2afdb5df33552758df9e2718cc20
Job <jobtalle@hotmail.com>
2020-05-22 14:32:42 +0200

    Implemented CSS color source

--- js/render/color.js
+++ js/render/color.js
@@ -13,6 +13,32 @@ const Color = function(r, g, b, a = 1) {
     this.a = a;
 };
 
+/**
+ * Make a color from a CSS file
+ * @param name
+ */
+Color.fromCSS = function(name) {
+    const value = getComputedStyle(document.body).getPropertyValue("--color-" + name).trim();
+
+    if (value.charAt(0) === "#") {
+        const integer = parseInt(value.substr(1), 16);
+
+        if (integer & 0xFF000000)
+            return new Color(
+                ((integer >> 24) & 0xFF) / 0xFF,
+                ((integer >> 16) & 0xFF) / 0xFF,
+                ((integer >> 8) & 0xFF) / 0xFF,
+                (integer & 0xFF) / 0xFF);
+        else
+            return new Color(
+                ((integer >> 16) & 0xFF) / 0xFF,
+                ((integer >> 8) & 0xFF) / 0xFF,
+                (integer & 0xFF) / 255);
+    }
+
+    // TODO: support rgba, rgb
+};
+
 Color.RED = new Color(1, 0, 0);
 Color.GREEN = new Color(0, 1, 0);
 Color.BLUE = new Color(0, 0, 1);
--- js/render/sand.js
+++ js/render/sand.js
@@ -12,7 +12,7 @@ const Sand = function(gl, randomSource) {
         gl,
         this.SHADER_VERTEX,
         this.SHADER_FRAGMENT,
-        ["scale"],
+        ["scale", "color"],
         ["position"]);
 
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffer);
@@ -21,6 +21,8 @@ const Sand = function(gl, randomSource) {
         new Float32Array([-1, -1, -1, 1, 1, 1, 1, -1]),
         gl.STATIC_DRAW);
 };
+
+Sand.prototype.COLOR = Color.fromCSS("sand");
 
 Sand.prototype.SHADER_VERTEX = `#version 100
 attribute vec2 position;
@@ -33,11 +35,10 @@ void main() {
 Sand.prototype.SHADER_FRAGMENT = `#version 100
 ` + CommonShaders.cubicNoise + `
 uniform mediump float scale;
+uniform lowp vec3 color;
 
 void main() {
-  mediump vec3 sandy = vec3(196.0 / 255.0, 147.0 / 255.0, 74.0 / 255.0);
-  
-  gl_FragColor = vec4(sandy * (pow(random2(gl_FragCoord.xy), 9.0) * 0.4 * cubicNoise(vec3(1.5 * gl_FragCoord.xy / scale, 0.0)) + 0.9), 1.0);
+  gl_FragColor = vec4(color * (pow(random2(gl_FragCoord.xy), 9.0) * 0.4 * cubicNoise(vec3(1.5 * gl_FragCoord.xy / scale, 0.0)) + 0.9), 1.0);
 }
 `;
 
@@ -52,6 +53,7 @@ Sand.prototype.write = function(scale) {
     this.gl.bindTexture(this.gl.TEXTURE_2D, this.randomSource.texture);
 
     this.gl.uniform1f(this.program.uScale, scale);
+    this.gl.uniform3f(this.program.uColor, this.COLOR.r, this.COLOR.g, this.COLOR.b);
 
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffer);
     this.gl.enableVertexAttribArray(this.program.aPosition);
