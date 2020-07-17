7f19c011901cb58a8e1842ae837981c5a8371b16
Job <jobtalle@hotmail.com>
2020-07-17 14:51:25 +0200

    Detected WebGL unavailability & implemented autosave

--- js/main.js
+++ js/main.js
@@ -8,110 +8,119 @@ const gl =
     canvas.getContext("webgl", glParameters) ||
     canvas.getContext("experimental-webgl", glParameters);
 
-// Enable VAO
-gl.vao = gl.getExtension("OES_vertex_array_object");
+if (gl) {
+    // Enable VAO
+    gl.vao = gl.getExtension("OES_vertex_array_object");
 
-// Enable 32 bit element indices
-gl.getExtension("OES_element_index_uint");
+    // Enable 32 bit element indices
+    gl.getExtension("OES_element_index_uint");
 
-const sessionData = window["localStorage"].getItem("session");
-let session = new Session(); // TODO: Make this const, it's variable for debugging only
+    const sessionData = window["localStorage"].getItem("session");
+    const session = new Session();
 
-const onDeserializationError = () => {
-    window["localStorage"].removeItem("session");
+    const save = () => {
+        window["localStorage"].setItem("session", session.serialize(koi).toString());
+    };
 
-    location.reload();
-};
+    const onDeserializationError = () => {
+        window["localStorage"].removeItem("session");
 
-// Retrieve last session if it exists
-if (sessionData) {
-    try {
-        session.deserialize(new BinBuffer(sessionData));
+        location.reload();
+    };
+
+    // Retrieve last session if it exists
+    if (sessionData) {
+        try {
+            session.deserialize(new BinBuffer(sessionData));
+        }
+        catch(error) {
+            onDeserializationError();
+        }
     }
-    catch(error) {
+
+    const systems = new Systems(gl, new Random(session.environmentSeed), canvas.width, canvas.height);
+    let lastDate = null;
+    let koi = null;
+    let loaded = true;
+
+    const resize = () => {
+        const wrapper = document.getElementById("wrapper");
+
+        canvas.width = wrapper.offsetWidth;
+        canvas.height = wrapper.offsetHeight;
+
+        systems.resize(canvas.width, canvas.height);
+
+        if (koi)
+            koi.resize();
+    };
+
+    window.onresize = resize;
+
+    resize();
+
+    try {
+        koi = session.makeKoi(systems);
+    }
+    catch (error) {
         onDeserializationError();
     }
+
+    lastDate = new Date();
+
+    const loop = () => {
+        if (loaded) {
+            const date = new Date();
+
+            koi.render(.001 * (date - lastDate));
+            lastDate = date;
+
+            requestAnimationFrame(loop);
+        }
+    };
+
+    requestAnimationFrame(loop);
+
+    // Autosave
+    setInterval(save, 60000);
+
+    canvas.addEventListener("mousedown", event => {
+        event.preventDefault();
+
+        koi.touchStart(event.clientX, event.clientY);
+    });
+
+    canvas.addEventListener("touchstart", event => {
+        event.preventDefault();
+
+        koi.touchStart(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
+    });
+
+    canvas.addEventListener("mousemove", event => {
+        koi.touchMove(event.clientX, event.clientY);
+    });
+
+    canvas.addEventListener("touchmove", event => {
+        event.preventDefault();
+
+        koi.touchMove(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
+    })
+
+    canvas.addEventListener("mouseup", () => {
+        koi.touchEnd();
+    });
+
+    canvas.addEventListener("touchend", event => {
+        event.preventDefault();
+
+        koi.touchEnd();
+    });
+
+    window.onbeforeunload = () => {
+        save();
+
+        koi.free();
+        systems.free();
+        loaded = false;
+    };
 }
-
-const systems = new Systems(gl, new Random(session.environmentSeed), canvas.width, canvas.height);
-let lastDate = null;
-let koi = null;
-let loaded = true;
-
-const resize = () => {
-    const wrapper = document.getElementById("wrapper");
-
-    canvas.width = wrapper.offsetWidth;
-    canvas.height = wrapper.offsetHeight;
-
-    systems.resize(canvas.width, canvas.height);
-
-    if (koi)
-        koi.resize();
-};
-
-window.onresize = resize;
-
-resize();
-
-try {
-    koi = session.makeKoi(systems);
-}
-catch (error) {
-    onDeserializationError();
-}
-
-lastDate = new Date();
-
-const loop = () => {
-    if (loaded) {
-        const date = new Date();
-
-        koi.render(.001 * (date - lastDate));
-        lastDate = date;
-
-        requestAnimationFrame(loop);
-    }
-};
-
-requestAnimationFrame(loop);
-
-canvas.addEventListener("mousedown", event => {
-    event.preventDefault();
-
-    koi.touchStart(event.clientX, event.clientY);
-});
-
-canvas.addEventListener("touchstart", event => {
-    event.preventDefault();
-
-    koi.touchStart(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
-});
-
-canvas.addEventListener("mousemove", event => {
-    koi.touchMove(event.clientX, event.clientY);
-});
-
-canvas.addEventListener("touchmove", event => {
-    event.preventDefault();
-
-    koi.touchMove(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
-})
-
-canvas.addEventListener("mouseup", () => {
-    koi.touchEnd();
-});
-
-canvas.addEventListener("touchend", event => {
-    event.preventDefault();
-
-    koi.touchEnd();
-});
-
-window.onbeforeunload = () => {
-    window["localStorage"].setItem("session", session.serialize(koi).toString());
-
-    koi.free();
-    systems.free();
-    loaded = false;
-};
