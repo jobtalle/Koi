9ca525349e7ebc7ab188b99f5ccfea7a1867bef2
Job <jobtalle@hotmail.com>
2020-08-04 12:24:52 +0200

    Fixed body mutator bug

--- js/koi/fish/breeder/mutators/mutatorFishBody.js
+++ js/koi/fish/breeder/mutators/mutatorFishBody.js
@@ -17,8 +17,8 @@ MutatorFishBody.prototype.SAMPLER_RADIUS = new SamplerPlateau(-40, 0, 40, 1);
  * @returns {FishBody} The mutated fish body
  */
 MutatorFishBody.prototype.mutate = function(random) {
-    this.body.length = this.mutateUint8(this.body.length, this.SAMPLER_LENGTH, random);
-    this.body.radius = this.mutateUint8(this.body.radius, this.SAMPLER_RADIUS, random);
+    this.body.length = this.mutateUint8(this.body.length, this.SAMPLER_LENGTH, random.getFloat());
+    this.body.radius = this.mutateUint8(this.body.radius, this.SAMPLER_RADIUS, random.getFloat());
 
     return this.body;
 };
