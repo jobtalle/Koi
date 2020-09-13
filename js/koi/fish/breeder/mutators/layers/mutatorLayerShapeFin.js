20866ed841bef257a8f628b2c813b594309d6586
Job <jobtalle@hotmail.com>
2020-09-13 13:33:57 +0200

    Fin shape mutates

--- js/koi/fish/breeder/mutators/layers/mutatorLayerShapeFin.js
+++ js/koi/fish/breeder/mutators/layers/mutatorLayerShapeFin.js
@@ -8,11 +8,12 @@ const MutatorLayerShapeFin = function(layer) {
 };
 
 MutatorLayerShapeFin.prototype = Object.create(Mutator.prototype);
+MutatorLayerShapeFin.prototype.SAMPLER_ROUNDNESS = new SamplerPlateau(-8, 0, 8, 1);
 
 /**
  * Mutate the layer
  * @param {Random} random A randomizer
  */
 MutatorLayerShapeFin.prototype.mutate = function(random) {
-    // TODO: Mutate
+    this.layer.roundness = this.mutateUint8(this.layer.roundness, this.SAMPLER_ROUNDNESS, random.getFloat());
 };
