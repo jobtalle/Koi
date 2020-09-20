25823644786a3bfca42ada9a50b9ee8fce0ecd3e
Job <jobtalle@hotmail.com>
2020-09-20 14:14:35 +0200

    Removed unnecessary mover code

--- js/koi/mover.js
+++ js/koi/mover.js
@@ -60,20 +60,20 @@ Mover.prototype.render = function(
  * @param {Air} air The air to displace
  */
 Mover.prototype.touchMove = function(x, y, air) {
-    this.cursorPrevious.set(this.cursor);
-    this.cursor.x = x;
-    this.cursor.y = y;
-
     if (this.move) {
+        this.cursorPrevious.set(this.cursor);
+        this.cursor.x = x;
+        this.cursor.y = y;
+
         this.cursorOffset.set(this.cursor).add(this.offset);
         this.move.moveTo(this.cursorOffset);
-    }
 
-    if (this.touch) {
-        const intensity = this.AIR_INTENSITY * (this.cursor.x - this.cursorPrevious.x);
+        if (this.touch) {
+            const intensity = this.AIR_INTENSITY * (this.cursor.x - this.cursorPrevious.x);
 
-        // TODO: This makes very large steps, smooth this out
-        air.addDisplacement(x, y + this.AIR_HEIGHT, this.AIR_RADIUS, intensity);
+            // TODO: This makes very large steps, smooth this out
+            air.addDisplacement(x, y + this.AIR_HEIGHT, this.AIR_RADIUS, intensity);
+        }
     }
 };
 
