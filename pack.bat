b2b02cc44361468277ab140618836792aa1b0ebc
Job <jobtalle@hotmail.com>
2020-11-20 21:08:29 +0100

    Electron packer skips more files

--- pack.bat
+++ pack.bat
@@ -1,2 +1,2 @@
 call build.bat
-call electron-packager . koi --platform=win32 --arch=x64 --overwrite --ignore="^/js|^/css|^/\.idea|pack\.bat|build\.bat|index\.html|README\.md"
+call electron-packager . koi --platform=win32 --arch=x64 --overwrite --ignore="^/js|^/css|^/node_modules|^/\.idea|pack\.bat|build\.bat|index\.html|README\.md|LICENSE|package-lock\.json"
