ac598e7d838dca0c7ba3c98420478210a4d535dc
Job <jobtalle@hotmail.com>
2020-12-05 17:18:06 +0100

    Card tutorial starts after a set amount of mutations has occurred

--- js/koi/tutorial/tutorialCards.js
+++ js/koi/tutorial/tutorialCards.js
@@ -11,3 +11,53 @@ const TutorialCards = function(overlay) {
 
 TutorialCards.prototype = Object.create(Tutorial.prototype);
 TutorialCards.prototype.PHASE_START = 0;
+TutorialCards.prototype.PHASE_WAITING = 1;
+TutorialCards.prototype.PHASE_CREATE_CARD = 2;
+TutorialCards.prototype.MUTATIONS_REQUIRED = 3;
+TutorialCards.prototype.LANG_CREATE_CARD = "TUTORIAL_CREATE_CARD";
+
+/**
+ * Cue the card mechanic tutorial
+ */
+TutorialCards.prototype.start = function() {
+    this.overlay.setText(language.get(this.LANG_CREATE_CARD));
+
+    this.phase = this.PHASE_CREATE_CARD;
+};
+
+/**
+ * A function that is called after a pattern mutation occurs
+ */
+TutorialCards.prototype.onMutate = function() {
+    if (this.mutations < this.MUTATIONS_REQUIRED) {
+        localStorage.setItem("tutorial", (++this.mutations).toString());
+
+        if (this.mutations === this.MUTATIONS_REQUIRED)
+            this.start();
+    }
+};
+
+/**
+ * Update the tutorial state
+ * @param {Constellation} constellation The constellation
+ * @param {Mover} mover The mover
+ * @returns {Boolean} True if the tutorial has finished
+ */
+TutorialCards.prototype.update = function(constellation, mover) {
+    switch (this.phase) {
+        case this.PHASE_START:
+            if (this.mutations < this.MUTATIONS_REQUIRED)
+                this.phase = this.PHASE_WAITING;
+            else if (this.mutations === this.MUTATIONS_REQUIRED)
+                this.start();
+            else
+                return true;
+
+            break;
+        case this.PHASE_CREATE_CARD:
+
+            break;
+    }
+
+    return false;
+};
