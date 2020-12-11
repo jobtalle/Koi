e24ff5870f393ba490e9a16248a7cb07536b9b82
Job <jobtalle@hotmail.com>
2020-12-11 18:37:07 +0100

    Fish icon has fins

--- js/koi/gui/cards/requirement/icon/fishIconLayer.js
+++ js/koi/gui/cards/requirement/icon/fishIconLayer.js
@@ -12,11 +12,16 @@ const FishIconLayer = function() {
 
 FishIconLayer.prototype.ID_MASK = "fish-icon-layer-mask";
 FishIconLayer.prototype.ID_BASE = "fish-icon-layer-pattern-base-";
+FishIconLayer.prototype.WIDTH = 30;
+FishIconLayer.prototype.HEIGHT = 70;
 FishIconLayer.prototype.EYE_RADIUS = 3;
 FishIconLayer.prototype.EYE_X = 10;
 FishIconLayer.prototype.EYE_Y = 10;
-FishIconLayer.prototype.WIDTH = 30;
-FishIconLayer.prototype.HEIGHT = 70;
+FishIconLayer.FIN_CENTER_X = FishIconLayer.prototype.WIDTH * .5;
+FishIconLayer.FIN_CENTER_Y = FishIconLayer.prototype.HEIGHT * .3;
+FishIconLayer.FIN_RADIUS = FishIconLayer.prototype.WIDTH * .5;
+FishIconLayer.FIN_ANGLE_START = 0;
+FishIconLayer.FIN_ANGLE_STOP = Math.PI * .35;
 
 /**
  * Make the fish icon layer mask
@@ -25,17 +30,43 @@ FishIconLayer.prototype.HEIGHT = 70;
 FishIconLayer.makeMask = function() {
     const mask = SVG.createMask(FishIconLayer.prototype.ID_MASK);
 
-    mask.appendChild(FishIconLayer.prototype.createBodyPath(
-        FishIconLayer.prototype.WIDTH,
-        FishIconLayer.prototype.HEIGHT));
+    mask.appendChild(FishIconLayer.prototype.createBodyPath());
+    mask.appendChild(SVG.createPath([
+        "M",
+        FishIconLayer.FIN_CENTER_X,
+        FishIconLayer.FIN_CENTER_Y,
+        "L",
+        FishIconLayer.FIN_CENTER_X + Math.cos(FishIconLayer.FIN_ANGLE_START) * FishIconLayer.FIN_RADIUS,
+        FishIconLayer.FIN_CENTER_Y + Math.sin(FishIconLayer.FIN_ANGLE_START) * FishIconLayer.FIN_RADIUS,
+        "A",
+        FishIconLayer.FIN_CENTER_X,
+        FishIconLayer.FIN_CENTER_Y,
+        0, 0, 1,
+        FishIconLayer.FIN_CENTER_X + Math.cos(FishIconLayer.FIN_ANGLE_STOP) * FishIconLayer.FIN_RADIUS,
+        FishIconLayer.FIN_CENTER_Y + Math.sin(FishIconLayer.FIN_ANGLE_STOP) * FishIconLayer.FIN_RADIUS,
+    ]));
+    mask.appendChild(SVG.createPath([
+        "M",
+        FishIconLayer.FIN_CENTER_X,
+        FishIconLayer.FIN_CENTER_Y,
+        "L",
+        FishIconLayer.FIN_CENTER_X - Math.cos(FishIconLayer.FIN_ANGLE_START) * FishIconLayer.FIN_RADIUS,
+        FishIconLayer.FIN_CENTER_Y + Math.sin(FishIconLayer.FIN_ANGLE_START) * FishIconLayer.FIN_RADIUS,
+        "A",
+        FishIconLayer.FIN_CENTER_X,
+        FishIconLayer.FIN_CENTER_Y,
+        0, 0, 0,
+        FishIconLayer.FIN_CENTER_X - Math.cos(FishIconLayer.FIN_ANGLE_STOP) * FishIconLayer.FIN_RADIUS,
+        FishIconLayer.FIN_CENTER_Y + Math.sin(FishIconLayer.FIN_ANGLE_STOP) * FishIconLayer.FIN_RADIUS,
+    ]));
 
     return mask;
 }
 
 /**
- * Make all pattern definitions
+ * Make all base pattern definitions
  */
-FishIconLayer.makeDefsPatterns = function() {
+FishIconLayer.makeDefsPatternsBase = function() {
     for (let paletteIndex = 0, colors = Palette.COLORS.length; paletteIndex < colors; ++paletteIndex) {
         const id = FishIconLayer.prototype.ID_BASE + paletteIndex.toString();
         const pattern = SVG.createPattern(id);
@@ -50,6 +81,13 @@ FishIconLayer.makeDefsPatterns = function() {
 };
 
 /**
+ * Make all pattern definitions
+ */
+FishIconLayer.makeDefsPatterns = function() {
+    FishIconLayer.makeDefsPatternsBase();
+};
+
+/**
  * Make all defs used by icon layers
  */
 FishIconLayer.makeDefs = function() {
@@ -60,15 +98,14 @@ FishIconLayer.makeDefs = function() {
 
 /**
  * Create a path representing the fish body
- * @param {String} [className] The class name
  * @returns {SVGPathElement} The path element
  */
-FishIconLayer.prototype.createBodyPath = function(className) {
+FishIconLayer.prototype.createBodyPath = function() {
     return SVG.createPath([
         "M", this.WIDTH * .5, 0,
         "C", this.WIDTH, 0, this.WIDTH, this.HEIGHT - this.WIDTH * .5, this.WIDTH * .5, this.HEIGHT,
         "C", 0, this.HEIGHT - this.WIDTH * .5, 0, 0, this.WIDTH * .5, 0
-    ], className);
+    ]);
 };
 
 /**
